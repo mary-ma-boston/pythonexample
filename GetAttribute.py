1e79840d9c8e2dff6bafcadf5badb09fced12cdf
mary-ma-boston <mary.ma.boston@gmail.com>
2020-10-19 19:42:06 -0400

    edit

--- GetAttribute.py
+++ GetAttribute.py
@@ -7,10 +7,10 @@ dr = webdriver.Chrome()
 file_path = 'file:///' + os.path.abspath('GetAttribute.html')
 dr.get(file_path)
 link = dr.find_element_by_id('tooltip')
-sleep(10)
+# sleep(10)
 # get the attribute of tooltip
 #print(link.get_attribute('data-original-title'))
-print(link.get_attribute('tooltip'))
+print(link.get_attribute('data-toggle'))
 # get the text 
 print(link.text)
 dr.quit()
